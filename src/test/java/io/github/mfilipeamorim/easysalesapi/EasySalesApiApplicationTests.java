79455df054f1e2e1aa36a332c0338a76f7129cc1
Matheus Amorim <90119863+mfilipeamorim@users.noreply.github.com>
2024-07-21 19:36:19 -0300

    Initial commit

---
+++ src/test/java/io/github/mfilipeamorim/easysalesapi/EasySalesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mfilipeamorim.easysalesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasySalesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
